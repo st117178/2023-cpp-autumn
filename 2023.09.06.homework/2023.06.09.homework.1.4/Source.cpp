3f5a822113784625b8972e24c3bc7638dbb76d63
st117178 <st117178@student.spbu.ru>
2023-09-06 22:10:50 +0300

    2023.06.09.homework1.2

---
+++ 2023.09.06.homework/2023.06.09.homework.1.4/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	cout << 99 * (n + 1);
+	return 0;
+}
---
+++ 2023.09.06.homework/2023.06.09.homework1.3/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int a = n / 10;
+	int b = a + 1;
+	cout << a * b << 25;
+	return  0;
+}
