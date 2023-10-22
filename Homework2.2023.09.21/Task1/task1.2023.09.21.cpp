a4115cbcc5e4d3c82ed6ec84382c70d1d25b09cb
st117178 <st117178@student.spbu.ru>
2023-10-23 00:34:39 +0300

    23.10.2023\commitHW

--- Homework2.2023.09.21/Task1/task1.2023.09.21.cpp
+++ Homework2.2023.09.21/Task1/task1.2023.09.21.cpp
@@ -4,11 +4,14 @@ int main(int argc, char* argv[])
 {
 	int a = 0;
 	std::cin >> a;
+
 	if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
 		std::cout << "YES";
 	}
+
 	else {
 		std::cout << "NO";
 	}
+
 	return 0;
 }
