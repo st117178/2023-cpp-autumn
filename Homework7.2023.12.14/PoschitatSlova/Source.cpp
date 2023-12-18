fa7f38d8e1fab8967ae185b4be8ffebba3ef8fa2
st117178 <st117178@student.spbu.ru>
2023-12-18 19:11:03 +0300

    18.12.2023\commitHW

--- Homework7.2023.12.14/PoschitatSlova/Source.cpp
+++ Homework7.2023.12.14/PoschitatSlova/Source.cpp
@@ -15,59 +15,59 @@ int main(int argc, char* argv[])
 	fgets(a, 255, f);
 	fclose(f);
 
-    f = fopen("out.txt", "w");
-    fprintf(f, "Count words:%d\n", slova(a));
+	f = fopen("out.txt", "w");
+	fprintf(f, "Count words:%d\n", slova(a));
 
-    char* sen = strtok(a, ".!?");
-    int maxWords = 0;
-    char* maxsen = nullptr;
+	char* sen = strtok(a, ".!?");
+	int maxWords = 0;
+	char* maxsen = nullptr;
 
-    while (sen != nullptr) {
-        int words = slova(sen);
-        if (words > maxWords) {
-            maxWords = words;
-            maxsen = sen;
-        }
+	while (sen != nullptr) {
+		int words = slova(sen);
+		if (words > maxWords) {
+			maxWords = words;
+			maxsen = sen;
+		}
 
-        sen = strtok(nullptr, ".!?");
-    }
+		sen = strtok(nullptr, ".!?");
+	}
 
-    if (maxsen != nullptr) {
-        fprintf(f, "Sentence with max words: %s\n",maxsen);
-    }
-    else {
-        fprintf(f, "Pysto\n");
-    }
+	if (maxsen != nullptr) {
+		fprintf(f, "Sentence with max words: %s\n", maxsen);
+	}
+	else {
+		fprintf(f, "Pysto\n");
+	}
 
-    fclose(f);
+	fclose(f);
 
 	return EXIT_SUCCESS;
 }
 
 int slova(char* a) {
-    int count = 0;
-    int i = 0;
-    int len = strlen(a);
+	int count = 0;
+	int i = 0;
+	int len = strlen(a);
 
-    while (i < len && a[i] == ' ') {
-        i++;
-    }
+	while (i < len && a[i] == ' ') {
+		i++;
+	}
 
-    while (i < len) {
-        if (a[i] == ' ') {
-            count++;
-            while (i < len && a[i] == ' ') {
-                i++;
-            }
-        }
-        else {
-            i++;
-        }
-    }
+	while (i < len) {
+		if (a[i] == ' ') {
+			count++;
+			while (i < len && a[i] == ' ') {
+				i++;
+			}
+		}
+		else {
+			i++;
+		}
+	}
 
-    if (i > 0 && a[i - 1] != ' ') {
-        count++;
-    }
+	if (i > 0 && a[i - 1] != ' ') {
+		count++;
+	}
 
-    return count;
+	return count;
 }
